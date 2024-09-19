7675cac706837e270df595dc6d8d7693ef7cbf48
a38930 <quannguyen16902hn@gmail.com>
2024-09-19 07:22:34 +0700

    update2

--- src/server.js
+++ src/server.js
@@ -1,33 +1,26 @@
-// server.js hoặc app.js (Tên file có thể tùy chỉnh)
 const express = require("express");
-const dotenv = require("dotenv"); // Đảm bảo bạn đã cài đặt dotenv để đọc biến môi trường
+const dotenv = require("dotenv");
 const db = require("./models/index");
-const authRoutes = require("./routes/publicRoute/authRoute"); // Import các route liên quan đến xác thực
-const protectedRoutes = require("./routes/protectedRoutes/protectedRoute"); // Import các route được bảo vệ
+const authRoutes = require("./routes/publicRoute/authRoute");
+const protectedRoutes = require("./routes/protectedRoutes/protectedRoute");
 
-dotenv.config(); // Đọc các biến môi trường từ file .env
+dotenv.config();
 
 const app = express();
 
-// Để ứng dụng hiểu dữ liệu JSON từ client
 app.use(express.json());
 
-// Đồng bộ hóa cơ sở dữ liệu và các bảng
 db.sequelize
-  .sync({ force: false }) // force: true drops and recreates tables
+  .sync({ force: false })
   .then(() => {
-    console.log("Database & tables dropped and recreated!");
+    console.log("Database & tables synced!");
   })
   .catch((err) => {
     console.error("Error syncing database:", err);
   });
 
-// Định nghĩa các route
 app.use("/auth", authRoutes);
 app.use("/api", protectedRoutes);
 
-// Khởi chạy server
-const PORT = process.env.PORT || 8080; // Sử dụng cổng từ biến môi trường hoặc cổng mặc định
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Export ứng dụng Express để Vercel có thể sử dụng nó
+module.exports = app;
